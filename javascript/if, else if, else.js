c8a4022cc7eb236edf2e736f66f332e25a9294e7
Armando R <armando.roman.jr20@gmail.com>
2023-09-05 09:59:12 -0500

    Creating js files

---
+++ javascript/if, else if, else.js
@@ -0,0 +1,13 @@
+// if, else if, else
+
+var place = 'second';
+
+if (place == 'first') {
+    console.log("gold")
+} else if (place == 'second'){
+    console.log("silver")
+} else if (place == 'third') {
+    console.log("bronze")
+} else {
+    console.log("no medal...")
+}
---
+++ javascript/switch statement.js
@@ -0,0 +1,23 @@
+// switch statement
+
+var place = "second";
+
+switch (place) {
+    /* we use case to set the 'condition' 
+    for the first test, and so on */
+    case 'first':
+        // once tested if the condition is met 
+        // we wil console.log
+        console.log("gold");
+        // we use break to stop or continue
+        // if the condition wasn't meet
+        break;
+    case 'second':
+        console.log("silver");
+        break;
+    case 'third':
+        console.log('bronze');
+        break;
+    default:
+        console.log("no medal...");
+}
