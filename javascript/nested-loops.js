97ec103b315df1201e883e41d2afc4142f403261
Armando R <armando.roman.jr20@gmail.com>
2023-09-05 11:49:17 -0500

    Pushing files

---
+++ javascript/nested-loops.js
@@ -0,0 +1,18 @@
+// for (var i = 2023; i < 2025; i++){
+//     console.log(i);
+//     for (var j = 6; j < 9; j++){
+//         console.log("----------", j);
+//     }
+// }
+
+for (var i = 1; i < 11; i++){
+    if(i = 1){
+        console.log("Gold medal");
+    } else if (i = 2) {
+        console.log("Silver medal");
+    } else if (i = 3){
+        console.log("Bronze medal");
+    } else {
+        console.log(i);
+    }
+}
