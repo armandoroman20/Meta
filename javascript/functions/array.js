e478e147a0e9295cea6b040a8e3a0ecbcf90cd78
Armando R <armando.roman.jr20@gmail.com>
2023-09-05 15:39:49 -0500

    Adding new files

---
+++ javascript/functions/array.js
@@ -0,0 +1,19 @@
+// We will convert this into an array
+
+// in this example we have train 1 
+// with these as it's carriages
+
+
+var carriage0 = "wheat";
+var carriage1 = "barley";
+var carriage2 = "salt";
+var carriage3 = "potato";
+var carriage4 = "rocks";
+
+// we have train 2 with 2 carriages, 
+// but we cant use the name carriage1 or carriage2
+// we solve this by usong an array
+
+var train1 = ["wheat", "barley", "salt", "potato", "rocks"]
+
+var train2 = ["hops", "brew"];
