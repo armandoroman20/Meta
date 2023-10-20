525efac2f400232e8de09b0a5a8c90e10911edf5
Armando R <armando.roman.jr20@gmail.com>
2023-10-20 15:26:01 -0500

    Edited the for of and for in loop example

--- javascript/OOP/for-of-loops-and-objects.js
+++ javascript/OOP/for-of-loops-and-objects.js
@@ -1,13 +1,22 @@
+
 const car = {
     engine: true, 
     steering: true, 
     speed: "slow"
 }
 
-
 const sportsCar = Object.create(car);
 sportsCar.speed = "fast";
-console.log("The sportsCar object: ", sportsCar);
 
+// This is how to loop through an object and it's prototype
 
+for (prop in sportsCar) {
+    console.log(prop);
+}
+
+// This is how to loop through an object
+
+for (prop of Object.keys(sportsCar)) {
+    console.log(prop + ": " + sportsCar[prop]);
+}
 
